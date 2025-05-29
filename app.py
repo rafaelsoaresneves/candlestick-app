3f7a79e5f8f1dc7a29e7652a5944013d463f8822
rafaelsoaresneves <rafael.soares.neves@gmail.com>
2025-05-29 17:58:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -109,11 +109,11 @@ def get_data(ticker_, interval_, period_days_):
 # Carregando os dados
 try:
     ticker = yf.Ticker(symbol)
-    #data = ticker.history(period="2mo")
+    data = ticker.history(period="3mo")
 
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=period_days)
-    data = yf.download("AAPL", start="2025-01-01", end="2025-05-28", interval="1d")
+    #end_date = datetime.now()
+    #start_date = end_date - timedelta(days=period_days)
+    #data = yf.download("AAPL", start="2025-01-01", end="2025-05-28", interval="1d")
     
     #data = get_data(symbol, interval, period_days)
     
